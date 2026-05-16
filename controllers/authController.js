8fc4f105f05704d515a43ff43f77932f2bb45e04
Raghav Bhanot <rbraghav05@gmail.com>
2026-05-16 06:58:00 +0530

    Fixed nodemailer SMTP configuration

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,7 +7,9 @@ const nodemailer = require("nodemailer");
 
 /* mailer */
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.SMTP_UNAME,
         pass: process.env.SMTP_PASS
