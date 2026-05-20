0e315f2a49265013fb3770ad8fb67ee1a40a4ccc
Raghav Bhanot <rbraghav05@gmail.com>
2026-05-20 16:51:32 +0530

    brevo smtp config 3

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,7 +8,7 @@ const nodemailer = require("nodemailer");
 /* mailer */
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: 587,
+    port: process.env.SMTP_PORT,
     secure: false,
     auth: {
         user: process.env.SMTP_USER,
