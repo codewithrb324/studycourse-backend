9b7877bbeb124893abe935f63c33ab51f0b2aab9
Raghav Bhanot <rbraghav05@gmail.com>
2026-05-21 10:14:48 +0530

    fix transporter to sendEmail in authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -43,19 +43,17 @@ exports.signup = async (req, res) => {
 
         res.send({ code: 1 });
 
-        transporter.sendMail({
-            from: process.env.MAIL_SENDER,
-            to: req.body.uname,
-            subject: 'Activation mail from StudyCourse',
-            html: `Dear ${req.body.pname},<br/><br/>
-        Thank you for registering on <b>Study Course</b>!<br/><br/>
-        Click the link below to activate your account:<br/><br/>
-        <a href="${process.env.CLIENT_URL}/activateaccount?id=${code}">
-            ${process.env.CLIENT_URL}/activateaccount?id=${code}
-        </a>
-        <br/><br/>
-        If you did not register, please ignore this email.<br/><br/>`
-        }).catch(err => console.log("Mail error:", err));
+        sendEmail(
+            req.body.uname,
+            'Activation mail from StudyCourse',
+            `Dear ${req.body.pname},<br/><br/>
+    Thank you for registering on <b>Study Course</b>!<br/><br/>
+    Click the link below to activate your account:<br/><br/>
+    <a href="${process.env.CLIENT_URL}/activateaccount?id=${code}">
+        ${process.env.CLIENT_URL}/activateaccount?id=${code}
+    </a><br/><br/>
+    If you did not register, please ignore this email.<br/><br/>`
+        );
 
     } catch (err) {
         console.log(err.message);
@@ -102,10 +100,10 @@ exports.login = async (req, res) => {
             { expiresIn }
         );
 
-         res.cookie("authToken", jtoken, {
+        res.cookie("authToken", jtoken, {
             httpOnly: true,
             secure: process.env.COOKIE_SECURE === "true",
-            sameSite: process.env.COOKIE_SAMESITE || "lax", 
+            sameSite: process.env.COOKIE_SAMESITE || "lax",
             maxAge,
         });
 
@@ -152,12 +150,14 @@ exports.resend = async (req, res) => {
 
         res.send({ code: 1 });
 
-        transporter.sendMail({
-            from: process.env.MAIL_SENDER,
-            to: req.params.email,
-            subject: "Resend Activation Mail from StudyCourse",
-            html: `${process.env.CLIENT_URL}/activateaccount?id=${newCode}`
-        }).catch(err => console.log("Mail error:", err));
+        sendEmail(
+            req.params.email,
+            'Resend Activation Mail from StudyCourse',
+            `Click the link to activate your account:<br/><br/>
+    <a href="${process.env.CLIENT_URL}/activateaccount?id=${newCode}">
+        ${process.env.CLIENT_URL}/activateaccount?id=${newCode}
+    </a>`
+        );
 
     } catch (err) {
         console.log(err.message);
@@ -215,18 +215,16 @@ exports.forgot = async (req, res) => {
 
         await new Reset({ username: email, token, exptime }).save();
 
-        transporter.sendMail({
-            from: process.env.MAIL_SENDER,
-            to: email,
-            subject: 'Reset Password Mail from StudyCourse',
-            html: `Dear ${user.name},<br/><br/>
-        Click on the following link to reset your password<br/><br/>
-        <a href="${process.env.CLIENT_URL}/resetpassword?token=${token}">
-            ${process.env.CLIENT_URL}/resetpassword?token=${token}
-        </a>
-        <br/><br/>
-        This link is valid for 15 minutes only.<br/><br/>`
-        }).catch(err => console.log("Mail error:", err));
+        sendEmail(
+            email,
+            'Reset Password Mail from StudyCourse',
+            `Dear ${user.name},<br/><br/>
+    Click on the following link to reset your password:<br/><br/>
+    <a href="${process.env.CLIENT_URL}/resetpassword?token=${token}">
+        ${process.env.CLIENT_URL}/resetpassword?token=${token}
+    </a><br/><br/>
+    This link is valid for 15 minutes only.<br/><br/>`
+        );
 
         res.send({ code: 1 });
 
@@ -256,7 +254,7 @@ exports.reset = async (req, res) => {
             { pass: hash }
         );
 
-      await Reset.deleteOne({ token: req.body.token });
+        await Reset.deleteOne({ token: req.body.token });
 
         res.send({ code: 1 });
 
@@ -312,7 +310,7 @@ exports.searchUser = async (req, res) => {
         const user = await User.findOne({ username: req.params.email }).select("-pass");
         if (!user) return res.send({ code: 0 });
         res.send({ code: 1, udata: user });
-    } catch(e) {
+    } catch (e) {
         console.log(e.message);
         res.send({ code: 0 });
     }
@@ -328,7 +326,7 @@ exports.deleteMember = async (req, res) => {
         const result = await User.deleteOne({ _id: req.body.mid });
         res.send({ success: result.deletedCount === 1 });
 
-    } catch(e) {
+    } catch (e) {
         console.log(e.message);
         res.send({ success: false });
     }
@@ -347,12 +345,12 @@ exports.createAdmin = async (req, res) => {
             phone: req.body.phone,
             username: req.body.uname,
             pass: hash,
-            usertype: "admin",  
-            isActivated: true     
+            usertype: "admin",
+            isActivated: true
         }).save();
 
         res.send({ code: 1 });
-    } catch(e) {
+    } catch (e) {
         console.log(e.message);
         res.send({ code: 0 });
     }
