7620feceb8bf824153a1767de331c0e97e6d7a7d
Raghav Bhanot <rbraghav05@gmail.com>
2026-05-21 21:38:26 +0530

    Fix contact mail delivery

--- utils/mailer.js
+++ utils/mailer.js
@@ -5,24 +5,67 @@ const mailjet = Mailjet.apiConnect(
     process.env.MJ_APIKEY_PRIVATE
 );
 
-const sendEmail = async (to, subject, html) => {
+const sendEmail = async (
+    to,
+    subject,
+    html,
+    replyEmail = "",
+    replyName = ""
+) => {
     try {
-        await mailjet.post("send", { version: "v3.1" }).request({
+
+        const payload = {
             Messages: [
                 {
                     From: {
                         Email: process.env.MAIL_SENDER,
                         Name: "StudyCourse"
                     },
-                    To: [{ Email: to }],
+
+                    To: [
+                        {
+                            Email: to
+                        }
+                    ],
+
                     Subject: subject,
-                    HTMLPart: html
+
+                    HTMLPart: html,
+
+                    TextPart: html
+                        .replace(/<br\s*\/?>/gi, "\n")
+                        .replace(/<[^>]*>/g, ""),
+
+                    ...(replyEmail && {
+                        ReplyTo: {
+                            Email: replyEmail,
+                            Name: replyName || "User"
+                        }
+                    })
                 }
             ]
-        });
-        console.log("Email sent to:", to);
+        };
+
+        const result = await mailjet
+            .post("send", { version: "v3.1" })
+            .request(payload);
+
+        console.log("EMAIL SENT");
+        console.log(JSON.stringify(result.body, null, 2));
+
+        return result;
+
     } catch (err) {
-        console.log("Mail error:", err.statusCode, err.message);
+
+        console.log("MAIL ERROR:");
+
+        if (err.response && err.response.body) {
+            console.log(JSON.stringify(err.response.body, null, 2));
+        } else {
+            console.log(err);
+        }
+
+        throw err;
     }
 };
 
