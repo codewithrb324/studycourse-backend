7d4951f6313146ea42fd316f3f233d39e972e9c1
Raghav Bhanot <rbraghav05@gmail.com>
2026-05-21 19:34:42 +0530

    contact controller updated

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -27,7 +27,7 @@ exports.contactUs = async (req, res) => {
         await Contact.create({ name, email, phone, message });
 
         await sendEmail(
-            "raghavbhanot908@gmail.com",
+             process.env.MAIL_SENDER,
             "Message from Website - Contact Us",
             `<b>Name:-</b> ${name}<br/><b>Phone:-</b> ${phone}<br/><b>Email:-</b> ${email}<br/><b>Message:-</b> ${message}`
         );
